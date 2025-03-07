d607dc397e8dc180ccf9d56c63a86c820d8a594a
Vedaa Anand <work.vedaa@gmail.com>
2025-03-07 20:23:04 +0530

    feat: move into folder & fix: title > text

---
+++ src/components/ComponentTitles.jsx
@@ -0,0 +1,5 @@
+function ComponentTitles({ text }) {
+  return <h3>{text}</h3>;
+}
+
+export default ComponentTitles;
--- src/components/WebAppName.jsx
+++ src/components/WebAppName.jsx
@@ -1,8 +1,10 @@
 function WebAppName() {
   return (
     <>
-      <h1>Lock&Key</h1>
-      <p>It's what's inside ✫</p>
+      <div className="title-subtitle">
+        <h1>Lock&Key</h1>
+        <p>It's what's inside ✫</p>
+      </div>
     </>
   );
 }
