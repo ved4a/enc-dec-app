fbbe42ee23cc1e53af938eb22819fae01ba6717a
Vedaa Anand <work.vedaa@gmail.com>
2025-03-21 17:52:46 +0530

    feat: finish playfair decryption

--- src/utils/decryptions.js
+++ src/utils/decryptions.js
@@ -1,6 +1,7 @@
 import { caesarEncryption } from "./encryptions";
 import { genKey } from "./encryptions";
 import { isLetter } from "./encryptions";
+import { removeDuplicateLetters } from "./encryptions";
 
 export function caesarDecryption(text, shift){
     return caesarEncryption(text, 26 - shift);
@@ -57,4 +58,53 @@ export function affineDecryption(text, a, b){
         }
     }
     return plaintext;
+}
+
+export function playfairDecryption(text, key) {
+    let result = "";
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    text = text.toUpperCase().split(" ").join("");
+    key = key.toUpperCase().split(" ").join("");
+
+    let arr = [];
+
+    function findPosition(letter) {
+        for (let row = 0; row < 5; row++) {
+            let col = arr[row].indexOf(letter);
+            if (col !== -1) return { row, col };
+        }
+        return null;
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let letters = [];
+        arr.push(letters);
+    }
+
+    let total = key + alphabet;
+    let encryptSquare = removeDuplicateLetters(total);
+
+    for (let i = 0; i < 25; i++) {
+        let index = Math.floor(i / 5);
+        arr[index].push(encryptSquare[i]);
+    }
+
+    for (let i = 0; i < text.length; i += 2) {
+        let a = text[i];
+        let b = text[i + 1];
+        let posA = findPosition(a);
+        let posB = findPosition(b);
+
+        if (posA.row === posB.row) {
+            result += arr[posA.row][(posA.col + 4) % 5];
+            result += arr[posB.row][(posB.col + 4) % 5];
+        } else if (posA.col === posB.col) {
+            result += arr[(posA.row + 4) % 5][posA.col];
+            result += arr[(posB.row + 4) % 5][posB.col];
+        } else {
+            result += arr[posA.row][posB.col];
+            result += arr[posB.row][posA.col];
+        }
+    }
+    return result;
 }
