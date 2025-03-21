f03a7d8302114c90aea29340e0bde07646e59507
Vedaa Anand <work.vedaa@gmail.com>
2025-03-21 17:57:29 +0530

    fix: replace filename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import WebAppName from "./components/WebAppName.jsx";
-import EncryptionApp from "./components/EncSelector.jsx";
+import EncryptionApp from "./components/EncryptionInput.jsx";
 
 function Page() {
   return (
