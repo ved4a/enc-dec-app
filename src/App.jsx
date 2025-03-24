46ad015d116805c51ae25dede6314abac2916591
Vedaa Anand <work.vedaa@gmail.com>
2025-03-24 18:55:23 +0530

    feat: add paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import WebAppName from "./components/WebAppName.jsx";
 import EncryptionApp from "./components/EncryptionInput.jsx";
+import ResultsPage from "./components/Results.jsx";
 
 function Page() {
   return (
     <>
       <WebAppName />
-      <EncryptionApp />
+      <Routes>
+        <Route path="/" element={<EncryptionApp />} />
+        <Route path="/result" element={<ResultsPage />} />
+      </Routes>
     </>
   );
 }
